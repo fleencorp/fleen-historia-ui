26b69ebdfe55a78209132203b93ba26e1a5d49fb
Yusuf Alamu Musa <volunux@gmail.com>
2023-12-05 00:52:18 +0100

    document ErrorResponse

--- src/app/model/response/error.response.ts
+++ src/app/model/response/error.response.ts
@@ -1,14 +1,63 @@
-import {AnyProp} from "../type";
+import {AnyObject} from "../type";
 
+/**
+ * @class ErrorResponse
+ * @description
+ *   Represents an error response received from an HTTP request.
+ * @author Yusuf Alamu Musa
+ * @version 1.0
+ */
 export class ErrorResponse {
 
+  /**
+   * @property message
+   * @description
+   *   The error message associated with the response.
+   */
   public message!: string;
+
+  /**
+   * @property status
+   * @description
+   *   The HTTP status code associated with the error response.
+   */
   public status?: number;
+
+  /**
+   * @property timestamp
+   * @description
+   *   The timestamp indicating when the error response occurred.
+   */
   public timestamp?: Date;
+
+  /**
+   * @property type
+   * @description
+   *   The type of error, if provided by the server.
+   */
   public type?: string | null;
-  public fields?: AnyProp[];
+
+  /**
+   * @property fields
+   * @description
+   *   Additional fields or details associated with the error response.
+   */
+  public fields?: AnyObject[];
+
+  /**
+   * @property path
+   * @description
+   *   The path or URI that caused the error, if available.
+   */
   public path?: string | null;
 
+  /**
+   * @constructor
+   * @description
+   *   Initializes a new instance of the ErrorResponse class.
+   *
+   * @param data - The data object containing information about the error response.
+   */
   public constructor(data: ErrorResponse) {
     this.message = data?.message ? data?.message : '';
     this.status = data?.status;
--- src/app/model/type/base.type.ts
+++ src/app/model/type/base.type.ts
@@ -1,16 +1,40 @@
+/**
+ * @typedef AnyArray
+ * @description
+ *   Represents an array that can have elements of varying types, such as [string, any][] or any.
+ */
 export type AnyArray = [string, any][] | any;
 
+/**
+ * @typedef AnyObject
+ * @description
+ *   Represents an object with string keys and values of any type.
+ */
 export type AnyObject = Record<string, any>;
 
-export type AnyProp = {
-  [key: string] : any
-}
+/**
+ * @typedef AnyRegEx
+ * @description
+ *   Represents an object with properties of any type. Often used to store regular expressions.
+ */
+export type AnyRegEx = AnyObject;
 
-export type AnyRegEx = AnyProp;
-
+/**
+ * @typedef TwoArray
+ * @description
+ *   Represents an array that can have either two numbers, two strings, or be null.
+ */
 export type TwoArray = [number, number] | [string, string] | null;
 
+/**
+ * @typedef DependencyProvider
+ * @description
+ *   Represents a dependency provider configuration for use in Angular's dependency injection.
+ *
+ * @property {string} provide - The key used to provide the dependency in the dependency injection.
+ * @property {AnyObject} useValue - The actual value or object to be provided.
+ */
 export type DependencyProvider = {
   provide: string;
-  useValue: AnyProp;
+  useValue: AnyObject;
 }
