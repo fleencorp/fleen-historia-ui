674ab82228326c3e71ccac0f14f3ad741c52d069
Yusuf Alamu Musa <volunux@gmail.com>
2023-12-15 15:36:41 +0100

    add documentations

--- src/app/model/enum/mfa.enum.ts
+++ src/app/model/enum/mfa.enum.ts
@@ -1,10 +1,26 @@
 
+/**
+ * @enum MfaSetupStatus
+ * @description
+ *   Represents the status of Multi-Factor Authentication (MFA) setup.
+ *   - `COMPLETE`: MFA setup is complete.
+ *   - `IN_PROGRESS`: MFA setup is in progress.
+ */
 export enum MfaSetupStatus {
   COMPLETE = 'COMPLETE',
   IN_PROGRESS = 'IN_PROGRESS'
 }
 
 
+/**
+ * @enum MfaType
+ * @description
+ *   Represents the types of Multi-Factor Authentication (MFA) available.
+ *   - `PHONE`: MFA using phone.
+ *   - `EMAIL`: MFA using email.
+ *   - `AUTHENTICATOR`: MFA using an authenticator app.
+ *   - `NONE`: No MFA configured.
+ */
 export enum MfaType {
   PHONE = 'PHONE',
   EMAIL = 'EMAIL',
