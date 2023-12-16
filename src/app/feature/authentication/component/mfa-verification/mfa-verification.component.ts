e5246e9165037a8a287a6fd0fd81567a636c92aa
Yusuf Alamu Musa <volunux@gmail.com>
2023-12-16 22:54:10 +0100

    refactor mfa verification component

--- src/app/feature/authentication/component/mfa-verification/mfa-verification.component.ts
+++ src/app/feature/authentication/component/mfa-verification/mfa-verification.component.ts
@@ -6,6 +6,11 @@ import {AuthenticationService} from "../../service/authentication.service";
 import {ResendVerificationCodePayload} from "@app/model/type";
 import {FleenResponse} from "@app/model/response";
 
+/**
+ * Component for Multi-Factor Authentication (MFA) verification, extending the base component for MFA OTP functionality.
+ *
+ * @author Yusuf Alamu Musa
+ */
 @Component({
   selector: 'app-mfa-verification',
   templateUrl: './mfa-verification.component.html',
@@ -13,34 +18,73 @@ import {FleenResponse} from "@app/model/response";
 })
 export class MfaVerificationComponent extends MfaOtpBaseComponent  {
 
+  /**
+   * Input property for the MFA type, indicating the specific MFA method (PHONE, EMAIL, AUTHENTICATOR, NONE).
+   */
   @Input('mfa-type') public mfaType: MfaType = MfaType.NONE;
 
+  /**
+   * Constructs an instance of MfaVerificationComponent.
+   * @param authenticationService - The AuthenticationService for handling authentication-related operations.
+   */
   public constructor(private authenticationService: AuthenticationService) {
     super();
   }
 
+  /**
+   * Handles the submission of the MFA verification form. Emits the MFA code if valid.
+   * @param event - The event triggering the submission.
+   */
   public submit(event: Event): void {
     this.stopEvent(event);
+
     if (this.otp.valid) {
       const code: string = this.otp.value.toString();
-      this.otpSubmitted.emit({code, mfaType: this.mfaType, type: AuthVerificationType.MFA });
+      this.otpSubmitted.emit({
+        code,
+        mfaType: this.mfaType,
+        type: AuthVerificationType.MFA });
     }
   }
 
+  /**
+   * Overrides the base component method to set a custom verification message based on the MFA type.
+   */
   protected override setVerificationMessage(): void {
-    let verificationMessage: string = this.mfaType === MfaType.AUTHENTICATOR
-      ? 'Use an authenticator code to complete the process'
-      : 'Code has been sent to your ';
+    let verificationMessage: string;
 
-    this.verificationMessage = this.mfaType === MfaType.EMAIL
-      ? verificationMessage.concat(`email address ${this.emailAddress}`)
-      : verificationMessage.concat(`phone number ${this.phoneNumber}`);
+    if (this.mfaType === MfaType.AUTHENTICATOR) {
+      verificationMessage = 'Use an authenticator code to complete the process';
+    } else {
+      const contactInfo: string | undefined =
+        this.mfaType === MfaType.EMAIL ? this.emailAddress : this.phoneNumber;
+
+      verificationMessage = `Code has been sent to your ${this.getTypeLabel()} ${contactInfo}`;
+    }
+
+    this.verificationMessage = verificationMessage;
   }
 
+  /**
+   * Gets the label for the current MFA type.
+   * @returns The label for the current MFA type.
+   */
+  private getTypeLabel(): string {
+    return this.mfaType === MfaType.EMAIL ? 'email address' : 'phone number';
+  }
+
+  /**
+   * Determines whether the code can be resent, excluding authenticator method.
+   */
   get canResendCode(): boolean {
     return this.mfaType !== MfaType.AUTHENTICATOR;
   }
 
+  /**
+   * Overrides the base component method to handle service-specific OTP resend.
+   * @param resendVerificationPayload - The payload for resending the OTP.
+   * @returns An Observable of FleenResponse.
+   */
   override serviceResendOtp(resendVerificationPayload: ResendVerificationCodePayload): Observable<FleenResponse> {
     return this.authenticationService.resendPreAuthenticationOtp(resendVerificationPayload);
   }
