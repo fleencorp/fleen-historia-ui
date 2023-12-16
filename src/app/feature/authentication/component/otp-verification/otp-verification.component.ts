7613831371ecc759d3f2acff2fcc9df3f067b574
Yusuf Alamu Musa <volunux@gmail.com>
2023-12-16 22:48:34 +0100

    refactor otp verification component

--- src/app/feature/authentication/component/otp-verification/otp-verification.component.ts
+++ src/app/feature/authentication/component/otp-verification/otp-verification.component.ts
@@ -5,7 +5,13 @@ import {ResendVerificationCodePayload} from "@app/model/type";
 import {AuthenticationService} from "../../service";
 import {AuthVerificationType, VerificationType} from "@app/model/enum";
 import {FleenResponse} from "@app/model/response";
+import {isTruthy} from "@app/shared/helper";
 
+/**
+ * Component for OTP verification, extending the base component for MFA OTP functionality.
+ *
+ * @author Yusuf Alamu Musa
+ */
 @Component({
   selector: 'app-otp-verification',
   templateUrl: './otp-verification.component.html',
@@ -13,34 +19,74 @@ import {FleenResponse} from "@app/model/response";
 })
 export class OtpVerificationComponent extends MfaOtpBaseComponent {
 
+  /**
+   * Constructs an instance of OtpVerificationComponent.
+   * @param authenticationService - The AuthenticationService for handling authentication-related operations.
+   */
   public constructor(private authenticationService: AuthenticationService) {
     super();
   }
 
+  /**
+   * Handles the submission of the OTP form. Emits the OTP code if valid.
+   * @param event - The event triggering the submission.
+   */
   public submit(event: Event): void {
     this.stopEvent(event);
-    if (this.otp.valid) {
+
+    if (isTruthy(this.otp.valid) && this.otp.valid) {
       const code: string = this.otp.value.toString();
-      this.otpSubmitted.emit({code, verificationType: this.verificationType, type: AuthVerificationType.VERIFICATION });
+      this.otpSubmitted.emit({
+        code,
+        verificationType: this.verificationType,
+        type: AuthVerificationType.VERIFICATION
+      });
     }
   }
 
+  /**
+   * Switches to another verification method and resends OTP.
+   * @param event - The event triggering the method switch.
+   */
   public tryAnotherMethod(event: Event): void {
     this.stopEvent(event);
-    this.verificationType = this.verificationType !== VerificationType.EMAIL
-      ? VerificationType.EMAIL
-      : VerificationType.PHONE;
+
+    this.verificationType = this.toggleVerificationType(this.verificationType);
     this.resendOtp();
   }
 
-  protected override setVerificationMessage(): void {
-    let verificationMessage: string = 'Code has been sent to your '
-
-    this.verificationMessage = this.verificationType === VerificationType.EMAIL
-      ? verificationMessage.concat(`email address ${this.emailAddress}`)
-      : verificationMessage.concat(`phone number ${this.phoneNumber}`);
+  /**
+   * Toggles between email and phone verification types.
+   * @param currentType - The current verification type.
+   * @returns The toggled verification type.
+   */
+  private toggleVerificationType(currentType: VerificationType): VerificationType {
+    return currentType !== VerificationType.EMAIL ? VerificationType.EMAIL : VerificationType.PHONE;
   }
 
+  /**
+   * Sets the verification message based on the current verification type and contact information.
+   */
+  protected override setVerificationMessage(): void {
+    const contactInfo: string | undefined =
+      this.verificationType === VerificationType.EMAIL ? this.emailAddress : this.phoneNumber;
+
+    this.verificationMessage = `Code has been sent to your ${this.getTypeLabel()} ${contactInfo}`;
+  }
+
+  /**
+   * Gets the label for the current verification type.
+   * @returns The label for the current verification type.
+   */
+  private getTypeLabel(): string {
+    return this.verificationType === VerificationType.EMAIL ? 'email address' : 'phone number';
+  }
+
+  /**
+   * Overrides the base component method to handle service-specific OTP resend.
+   * @param resendVerificationPayload - The payload for resending the OTP.
+   * @returns An Observable of FleenResponse.
+   */
   override serviceResendOtp(resendVerificationPayload: ResendVerificationCodePayload): Observable<FleenResponse> {
     return this.authenticationService.resendOtp(resendVerificationPayload);
   }
