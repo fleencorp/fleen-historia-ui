75d1209c4ba89d8df9fa32c451bcb3fe7c619555
Yusuf Alamu Musa <volunux@gmail.com>
2023-12-17 16:13:34 +0100

    add documentation

--- src/app/feature/authentication/component/forgot-password/forgot-password.component.ts
+++ src/app/feature/authentication/component/forgot-password/forgot-password.component.ts
@@ -33,88 +33,181 @@ export class ForgotPasswordComponent extends BaseFormComponent implements OnInit
   public phoneNumber: string | undefined;
   protected formBuilder;
 
-  public constructor(protected authenticationService: AuthenticationService,
-                     protected router: Router) {
+  public constructor(
+      protected authenticationService: AuthenticationService,
+      protected router: Router) {
     super();
   }
 
   ngOnInit(): void {
-    this.emailAddress.addValidators([
-      Validators.required, Validators.email, Validators.minLength(4), Validators.maxLength(150)
-    ]);
-    this.verificationCode.addValidators([
-      Validators.required, Validators.minLength(1), Validators.maxLength(6), codeOrOtpValidator(VERIFICATION_CODE)
-    ]);
+    this.configureValidators();
   }
 
   protected override getRouter(): Router {
     return this.router;
   }
 
+  /**
+   * Handles the submission of a forgot password request.
+   *
+   * @param {Event} event - The event triggering the submission.
+   * @returns {void}
+   */
   public submit(event: Event): void {
     this.stopEvent(event);
+
     if (this.emailAddress.valid && isFalsy(this.isSubmitting)) {
       const emailAddress: string = this.emailAddress.value.toString();
       const payload: ForgotPasswordPayload = { emailAddress, verificationType: VerificationType.EMAIL };
-      this.authenticationService.clearAuthTokens();
-      this.disableSubmittingAndResetErrorMessage();
 
-      this.authenticationService.forgotPassword(payload)
-        .subscribe({
-          next: (result: ForgotPasswordResponse): void => {
-            this.phoneNumber = result.phoneNumber;
-            this.isDetailValid = true;
-          },
-          error: (result: ErrorResponse): void => {
-            this.handleError(result);
-          },
-          complete: (): void => {
-            this.enableSubmitting();
-          }
+      this.clearAuthTokensAndResetForm();
+
+      this.authenticationService.forgotPassword(payload).subscribe({
+        next: (result: ForgotPasswordResponse): void => {
+          this.handleForgotPasswordSuccess(result);
+        },
+        error: (result: ErrorResponse): void => {
+          this.handleError(result);
+        },
+        complete: (): void => {
+          this.enableSubmitting();
+        },
       });
     }
   }
 
+  /**
+   * Clears authentication tokens and resets the form.
+   *
+   * @returns {void}
+   * @private
+   */
+  private clearAuthTokensAndResetForm(): void {
+    this.authenticationService.clearAuthTokens();
+    this.disableSubmittingAndResetErrorMessage();
+  }
+
+  /**
+   * Handles the success response from the forgot password request.
+   *
+   * @param {ForgotPasswordResponse} result - The response from the forgot password request.
+   * @returns {void}
+   * @private
+   */
+  private handleForgotPasswordSuccess(result: ForgotPasswordResponse): void {
+    this.phoneNumber = result.phoneNumber;
+    this.isDetailValid = true;
+  }
+
+
+  /**
+   * Handles the submission of an OTP (One-Time Password) for password verification.
+   *
+   * @param {Event} event - The event triggering the submission.
+   * @returns {void}
+   */
   public submitOtp(event: Event): void {
     this.stopEvent(event);
+
     if (this.emailAddress.valid && this.verificationCode.valid && isFalsy(this.isSubmitting)) {
       const emailAddress: string = this.emailAddress.value.toString();
-      const payload: ResetPasswordPayload = { emailAddress, code: this.verificationCode.value };
+      const verificationCode: string = this.verificationCode.value;
+
+      const payload: ResetPasswordPayload = { emailAddress, code: verificationCode };
       this.disableSubmittingAndResetErrorMessage();
 
-      this.authenticationService.verifyResetPasswordCode(payload)
-        .subscribe({
-          next: (result: InitiatePasswordChangeResponse): void => {
-            this.authenticationService.clearAuthTokens();
-            this.authenticationService.saveAccessToken(result.accessToken);
-            this.isChangePasswordStage = true;
-          },
-          error: (result: ErrorResponse): void => {
-            this.handleError(result);
-          },
-          complete: (): void => {
-            this.enableSubmitting();
-          }
+      this.authenticationService.verifyResetPasswordCode(payload).subscribe({
+        next: (result: InitiatePasswordChangeResponse): void => {
+          this.handleVerificationSuccess(result);
+        },
+        error: (result: ErrorResponse): void => {
+          this.handleError(result);
+        },
+        complete: (): void => {
+          this.enableSubmitting();
+        },
       });
     }
   }
 
+  /**
+   * Handles the success response from the password verification.
+   *
+   * @param {InitiatePasswordChangeResponse} result - The response from the verification request.
+   * @returns {void}
+   * @private
+   */
+  private handleVerificationSuccess(result: InitiatePasswordChangeResponse): void {
+    this.clearAndSaveAccessToken(result.accessToken);
+    this.isChangePasswordStage = true;
+  }
+
+  /**
+   * Clears authentication tokens and saves the access token.
+   *
+   * @param {string} accessToken - The access token to save.
+   * @returns {void}
+   * @private
+   */
+  private clearAndSaveAccessToken(accessToken: string): void {
+    this.authenticationService.clearAuthTokens();
+    this.authenticationService.saveAccessToken(accessToken);
+  }
+
+  /**
+   * Initiates the process of changing the user's password.
+   *
+   * @param {ChangePasswordPayload} payload - The payload containing information needed for the password change.
+   * @returns {void}
+   */
   public changePassword(payload: ChangePasswordPayload): void {
     if (isFalsy(this.isSubmitting)) {
       this.disableSubmittingAndResetErrorMessage();
 
-      this.authenticationService.resetAndChangePassword(payload)
-        .subscribe({
-          error: (result: ErrorResponse): void => {
-            this.handleError(result);
-          },
-          complete: (): void => {
-            this.enableSubmitting();
-            this.authenticationService.clearAuthTokens();
-            this.authenticationService.startAuthentication(this.router);
-          }
+      this.authenticationService.resetAndChangePassword(payload).subscribe({
+        error: (errorResponse: ErrorResponse): void => { this.handleError(errorResponse); },
+        complete: (): void => { this.handlePasswordChangeCompletion(); },
       });
     }
   }
+
+  /**
+   * Handles the completion of the password change process.
+   *
+   * @returns {void}
+   * @private
+   */
+  private handlePasswordChangeCompletion(): void {
+    this.enableSubmitting();
+    this.clearAuthTokensAndStartAuthentication();
+  }
+
+  /**
+   * Clears authentication tokens and starts the authentication process.
+   *
+   * @returns {void}
+   * @private
+   */
+  private clearAuthTokensAndStartAuthentication(): void {
+    this.authenticationService.clearAuthTokens();
+    this.authenticationService.startAuthentication(this.router);
+  }
+
+  /**
+   * Configures validators for the email address and verification code.
+   *
+   * @returns {void}
+   * @private
+   */
+  private configureValidators(): void {
+    this.emailAddress.addValidators([
+      Validators.required, Validators.email, Validators.minLength(4), Validators.maxLength(150)
+    ]);
+
+    this.verificationCode.addValidators([
+      Validators.required, Validators.minLength(1), Validators.maxLength(6), codeOrOtpValidator(VERIFICATION_CODE)
+    ]);
+  }
+
 
 }
